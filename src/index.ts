bde00130f86603ac936918beec1f28a74a54bb35
YhyaGithub <yhyamobile57@gmail.com>
2023-03-03 11:25:51 +0200

    examples: add new example for custom functions

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import { createGlobalEnv } from "./interpreter/environment";
 import { evaluate } from "./interpreter//interpreter";
 import fs from "fs";
 
-run('./src/examples/conditions.rkz')
-
 function run(filename: string) {
   const parser = new Parser();
   const env = createGlobalEnv();
 
-  const input = fs.readFileSync(filename, 'utf8').toString();
+  const input = fs.readFileSync(filename, "utf8").toString();
   const program = parser.produceAST(input);
 
   evaluate(program, env);
